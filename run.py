46955da3aacfd2553dee101f5f84cece162c849a
TR4N5P4R3NT <tr4n5p4r3nt@kali>
2025-05-14 17:32:51 +0000

    Initial commit - W-YONKOS forum

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
-from app import app
+from app import app, db  # Make sure db is imported here
+from app import models   # Make sure this imports your models so SQLAlchemy sees them
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # ✅ Auto-create tables if they don't exist
     app.run(debug=True)
